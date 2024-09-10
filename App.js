767c6973107ddf89fac4df7b83b94252596f8a73
realguites2 <guilherme.tes@gmail.com>
2024-09-10 03:14:29 -0300

    app rodando

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import {
   View,
   Button,
   Alert,
+  StyleSheet,
 } from "react-native";
 import { BluetoothManager } from "react-native-bluetooth-escpos-printer";
 import {
@@ -20,7 +21,27 @@ import {
 } from "react-native-permissions";
 import ItemList from "./ItemList";
 import SamplePrint from "./SamplePrint";
-import { styles } from "./styles";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  containerList: { flex: 1, flexDirection: 'column' },
+  bluetoothStatusContainer: { justifyContent: 'flex-end', alignSelf: 'flex-end' },
+  bluetoothStatus: color => ({
+    backgroundColor: color,
+    padding: 8,
+    borderRadius: 2,
+    color: 'white',
+    paddingHorizontal: 14,
+    marginBottom: 20,
+  }),
+  bluetoothInfo: { textAlign: 'center', fontSize: 16, color: '#FFC806', marginBottom: 20 },
+  sectionTitle: { fontWeight: 'bold', fontSize: 18, marginBottom: 12 },
+  printerInfo: { textAlign: 'center', fontSize: 16, color: '#E9493F', marginBottom: 20 },
+});
 
 const App = () => {
   const [pairedDevices, setPairedDevices] = useState([]);
